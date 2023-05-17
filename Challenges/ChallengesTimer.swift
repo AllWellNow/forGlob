52f1f384d4de159b2d0e4d501c756b2b4659ca63
AllWellNow <112175849+AllWellNow@users.noreply.github.com>
2023-05-17 13:21:32 +0200

    Add files via upload

---
+++ Challenges/ChallengesTimer.swift
@@ -0,0 +1,39 @@
+//
+//  ChalengesTimer.swift
+//  IF project
+//
+//  Created by Misha on 26.01.2023.
+//
+
+import UIKit
+import JDFlipNumberView
+import TinyConstraints
+
+struct ChallengesTimer {
+    var timer: JDDateCountdownFlipView?
+    
+    mutating func startTimer(view: UIView, numberOfDays: Int, targetDate: Date) {
+        view.backgroundColor = .clear
+        
+        timer = JDDateCountdownFlipView(dayDigitCount: numberOfDays, imageBundle: JDFlipNumberViewImageBundle(named: "JDFlipNumberView", in: Bundle.main))
+        if let timer = timer {
+            
+            timer.targetDate = targetDate
+            timer.animationsEnabled = true
+            timer.backgroundColor = .clear
+            timer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+            timer.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            view.addSubview(timer)
+            view.autoresizesSubviews = true
+        }
+    }
+    
+    mutating func killTimer() {
+        if let timer = timer {
+            timer.targetDate = .now
+            timer.removeFromSuperview()
+        }
+    }
+    
+}
